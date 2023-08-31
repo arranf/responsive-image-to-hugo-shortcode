94f60d3749342cf429b8e039bc750be8c603c6cb
Arran France <arran@arranfrance.com>
2023-08-31 01:26:01 +0100

    fix: deserialization and serialization when hqimage field is missing

--- src/data.rs
+++ src/data.rs
@@ -6,5 +6,8 @@ pub struct Data {
     pub name: String,
     pub fallback: FallbackImage,
     pub sources: Vec<Source>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)] 
     pub hqimage: Option<String>
 }
